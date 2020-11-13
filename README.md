09e855c40a7729ac0b6ce869c2545795ffb26b7f
Jordi Prats <jordi.prats@gmail.com>
2020-11-13 19:27:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-awsh
+
+AWS ssh
---
+++ awstools.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+
+from configparser import ConfigParser
+
+import subprocess
+import boto3
+import click
+import sys
+import os
+
+debug = False
+set_profile = 'default'
+set_region = None
+
+def load_defaults(config_file):
+    global debug, set_profile, set_region
+
+    try:
+        config = ConfigParser()
+        config.read(config_file)
+
+        try:
+            debug = config.getboolean('awsh', 'debug')
+        except:
+            debug = False
+
+        try:
+            set_profile = config.get('aws', 'profile').strip('"').strip("'").strip()
+        except:
+            set_profile = 'default'
+
+        try:
+            set_region = config.get('aws', 'region').strip('"').strip("'").strip()
+        except:
+            set_region = None
+    except:
+        pass
+
+def aws_search_instances(name):
+    global debug, set_profile, set_region
+
+    try:
+        if set_region:
+            ec2 = boto3.client(service_name='ec2', region_name=set_region)
+        else:
+            ec2 = boto3.client(service_name='ec2')
+    except Exception as e:
+        sys.exit('ERROR: '+str(e))
+
+    if name:
+        filter = {}
+        filter['Name'] = 'tag:Name'
+        filter['Values'] = [ name ]
+        instance_filter = [ filter ]
+
+        response = ec2.describe_instances(Filters=instance_filter)
+    else:
+        response = ec2.describe_instances()
+
+    return response["Reservations"]
+
+
+@click.group()
+@click.option('--profile', default=None, help='AWS profile', type=str)
+@click.option('--region', default=None, help='AWS region', type=str)
+def awstools(profile, region):
+    global debug, set_profile, set_region
+
+    if profile:
+        os.environ["AWS_PROFILE"] = profile
+    else:
+        os.environ["AWS_PROFILE"] = set_profile
+    
+    set_region = region
+
+@awstools.command()
+@click.argument('name', default='')
+@click.option('--running', is_flag=True, default=False, help='show only running instances')
+def search(name, running):
+    global debug
+
+    reservations = aws_search_instances(name=None)
+
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            try:
+                name_found = False
+                for tag in instance['Tags']:
+                    if tag['Key']=='Name':
+                        name_found = True
+                        if name in tag['Value'] or not name:
+                            if running and instance['State']['Name']=='running':
+                                print("{: <60} {: <20} {: <20}".format(tag['Value'], instance['PrivateIpAddress'], instance['InstanceId'] ))
+                            else:
+                                print("{: <60} {: <20} {: <20} {: <20}".format(tag['Value'], instance['PrivateIpAddress'], instance['InstanceId'], instance['State']['Name']))
+                if not name_found:
+                            if running and instance['State']['Name']=='running':
+                                print("{: <60} {: <20} {: <20}".format('-', instance['PrivateIpAddress'], instance['InstanceId'] ))
+                            else:
+                                print("{: <60} {: <20} {: <20} {: <20}".format('-', instance['PrivateIpAddress'], instance['InstanceId'], instance['State']['Name']))
+            except:
+                pass
+    
+
+@awstools.command()
+@click.argument('host')
+def ssh(host):
+    for reservation in aws_search_instances(host):
+        for instance in reservation["Instances"]:
+            if instance['State']['Name']=='running':
+                try:
+                    subprocess.check_call(['ssh', instance['PrivateIpAddress']])
+                    return
+                except:
+                    return
+    sys.exit('Not found')
+
+if __name__ == '__main__':
+    load_defaults(os.path.join(os.getenv("HOME"), '.awsh/config'))
+    awstools()
